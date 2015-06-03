0a12dc7c6c133d7aab07f043b90ced066b12623d
ksilitonga@hotmail.com <ksilitonga@hotmail.com>
2015-06-03 11:01:21 +1000

    saved files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525010301) do
+ActiveRecord::Schema.define(version: 20150603003647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20150525010301) do
     t.string   "filename"
     t.text     "description"
     t.integer  "album_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "view",        default: 0
   end
 
 end
